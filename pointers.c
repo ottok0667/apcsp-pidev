14db4f2e8bd60372ad00f961a2625e347cd83aed
ottok0667 <desertlilies@earthlink.net>
2018-05-28 10:31:25 -0700

    Changed pointers.c to use the pointers to swap the variables rather than using the variables themselves to do so

---
+++ pointers.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int a;
+  int* ptrtoa;
+
+  ptrtoa = &a;
+
+  a = 5;
+  printf("The value of a is %d\n", a);
+
+  *ptrtoa = 6;
+  printf("The value of a is %d\n", a);
+
+  printf("The value of ptrtoa is %d\n", ptrtoa);
+  printf("It stores the value %d\n", *ptrtoa);
+  printf("The address of a is %d\n", &a);
+
+
+  float temp;
+  float d;
+  float e;
+  float* ptrtod;
+  float* ptrtoe;
+
+  ptrtod = &d;
+  ptrtoe = &e;
+
+  d = 4.73;
+  e = 21.9;
+
+  printf("\n\n\nThe value of float d is %f, and its address is %d\n", d, ptrtod);
+  printf("The value of float e is %f, and its address is %d\n\n", e, ptrtoe);
+
+  temp = *ptrtod;
+  *ptrtod = *ptrtoe;
+  *ptrtoe = temp;
+
+  printf("Now the value of float d is %f, and its address is %d\n", d, ptrtod);
+  printf("Now the value of float e is %f, and its address is %d\n", e, ptrtoe);
+}
